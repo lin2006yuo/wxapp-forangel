7729594590daace975a99c45ced035587b526aa5
lin2006yuo <547677504@qq.com>
2018-12-14 23:57:52 +0800

    v2

--- miniprogram/pages/homeSelect/homeSelect.js
+++ miniprogram/pages/homeSelect/homeSelect.js
@@ -6,7 +6,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    module_list: [
+      {
+        name: 'hunli',
+        text: '婚礼',
+        text_en: 'Wedding'
+      },
+      {
+        name: 'yuer',
+        text: '育儿',
+        text_en: 'Parenting'
+      },
+      {
+        name: 'youjiao',
+        text: '幼教',
+        text_en: 'Education'
+      },
+      {
+        name: 'yunyu',
+        text: '孕育',
+        text_en: 'Inoculation'
+      }
+    ]
   },
 
   /**
@@ -15,53 +36,7 @@ Page({
   onLoad: function (options) {
     console.log(app)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handletap(e) {
+    console.log(e)
   }
 })
